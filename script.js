61d47ca53c7b1773c52220a63112350315dc2d40
tmurray0809 <murra340@purdue.edu>
2026-06-15 02:25:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener('click', e => {
+
+        e.preventDefault();
+
+        const target = document.querySelector(
+            link.getAttribute('href')
+        );
+
+        if(target){
+
+            target.scrollIntoView({
+                behavior:'smooth',
+                block:'start'
+            });
+
+        }
+
+    });
+
+});
